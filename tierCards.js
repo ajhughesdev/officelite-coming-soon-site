15cae2802bbbd0e22d9fb30b1acacdf6e06e963c
Andrew J Hughes <andrew@ajhughes.dev>
2022-05-06 01:46:48 -0400

    dynamically rendering of card data from .json file

---
+++ tierCards.js
@@ -0,0 +1,56 @@
+const url = "tierData.json";
+const itemsContainer = document.querySelector("#items-container");
+
+function useState() {
+  let _state = null;
+
+  function getState() {
+    return _state;
+  }
+
+  function setState(data) {
+    _state = [...data];
+  }
+
+  return [getState, setState];
+}
+
+const [getState, setState] = useState();
+
+function cardTemplate(data) {
+  const { name, price, terms, details } = data;
+
+  return `
+  <article>
+    <h2>${name}</h2>
+    <ul>
+      <li>${price}</li>
+      <li>${terms}</li>
+      <li>${details.blurb1}</li>
+      <li>${details.storage}</li>
+      <li>${details.blurb2}</li>
+    </ul>
+    <button data-type="try-for-free">Try for Free</button>
+  </article>
+  `;
+}
+
+async function getData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function renderProjectsToDom(data) {
+  let items = data.map((item) => cardTemplate(item)).join("");
+  itemsContainer.innerHTML = items;
+}
+
+async function handleInitialLoad() {
+  const data = await getData(url);
+  setState(data);
+
+  renderProjectsToDom(getState());
+}
+
+window.addEventListener("DOMContentLoaded", handleInitialLoad);
